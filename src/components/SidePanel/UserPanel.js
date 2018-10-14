72a72581d2ba8db94510fbfa274fe455ecfde8bb
Hercha <jens.hedberg@medieinstitutet.se>
2018-10-14 02:26:19 +0200

    Change avatar

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -9,7 +9,14 @@ class UserPanel extends React.Component {
     modal: false,
     previewImage: '',
     croppedImage: '',
-    blob: ''
+    blob: null,
+    uploadedCroppedImage: '',
+    storageRef: firebase.storage().ref(),
+    userRef: firebase.auth().currentUser,
+    usersRef: firebase.database().ref('users'),
+    metadata: {
+      contentType: 'image/jpeg'
+    }
   };
 
   openModal = () => this.setState({ modal: true });
@@ -32,6 +39,45 @@ class UserPanel extends React.Component {
     }
   ];
 
+  uploadCroppedImage = () => {
+    const { storageRef, userRef, blob, metadata } = this.state;
+
+    storageRef
+      .child(`avatars/user-${userRef.uid}`)
+      .put(blob, metadata)
+      .then(snap => {
+        snap.ref.getDownloadURL().then(downloadURL => {
+          this.setState({ uploadedCroppedImage: downloadURL }, () =>
+          this.changeAvatar()
+          );
+        }); 
+      });
+  };
+
+  changeAvatar = () => {
+    this.state.userRef
+      .updateProfile({
+        photoURL: this.state.uploadedCroppedImage
+      })
+      .then(() => {
+        console.log('PhotoURL updated');
+        this.closeModal();
+      })
+      .catch(err => {
+        console.error(err);
+      })
+
+      this.state.usersRef
+      .child(this.state.user.uid)
+      .update({ avatar: this.state.uploadedCroppedImage })
+      .then(() => {
+        console.log('User avatar updated');
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  };
+
   handleChange = event => {
     const file = event.target.files[0];
     const reader = new FileReader();
@@ -50,7 +96,7 @@ class UserPanel extends React.Component {
         let imageUrl = URL.createObjectURL(blob);
         this.setState({
           croppedImage: imageUrl,
-          blob: ''
+          blob
         });
       });
     }
@@ -128,7 +174,7 @@ class UserPanel extends React.Component {
             </Grid>
           </Modal.Content>
           <Modal.Actions>
-            {croppedImage && <Button color="green" inverted>
+            {croppedImage && <Button color="green" inverted onClick={this.uploadCroppedImage}>
               <Icon name="save" /> Change Avatar
             </Button>}
             <Button color="green" inverted onClick={this.handleCropImage}>
